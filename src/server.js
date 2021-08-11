532e0e908ab1260a5c0e71ecfc74ed791b66c809
Camilo García <gcgarciab@unal.edu.co>
2021-08-10 20:39:57 -0500

    Base structure

---
+++ src/server.js
@@ -0,0 +1,4 @@
+module.exports = {
+  usersApiUrl: process.env.USERS_API_URL || 'https://dw5-users.herokuapp.com',
+  accountsApiUrl: process.env.ACCOUNTS_API_URL || 'https://accounts-dw5.herokuapp.com/', 
+}
